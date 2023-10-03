fb2b69d5d0ec39c3142db096275cc60e406ec869
sbondar <bondar.svitlana@yahoo.com>
2023-10-03 21:24:43 +0300

    added lives, score and pause/continue buttons

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const grid = document.querySelector('.grid')
+const allblocks_div = document.querySelector('.allblocks')
 const scoreDisplay = document.querySelector('#score')
+const live = document.querySelector('#lives')
 const blockWidth = 100
 const blockHeight = 20
 const ballDiameter = 20
 const boardWidth = 560
 const boardHeight = 300
+let isPaused = false
+let lives = 3
+let game_over = false
+let game_reset = true
+
 let timerId
 let xDirection = -2
 let yDirection = 2
 let score = 0
+
 const userStart = [230, 10]
 let currentPosition = userStart
+
 const ballStart = [270, 40]
 let ballCurrentPosition = ballStart
+
 //create Block
 class Block {
     //metric system for the blocks
@@ -23,6 +33,7 @@ class Block {
         this.topRight = [x + blockWidth, y + blockHeight]
     }
 }
+
 //all blocks
 const blocks = [
     new Block(10, 270),
@@ -41,32 +52,38 @@ const blocks = [
     new Block(340, 210),
     new Block(450, 210),
 ]
-//draw a block
+
+//draw a blockdocument.querySelector('#reset').onclick = function(){
+
 function addBlocks() {
     for (let i = 0; i < blocks.length; i++) {
         const block = document.createElement('div')
         block.classList.add('block')
         block.style.left = blocks[i].bottomLeft[0] + 'px'
         block.style.bottom = blocks[i].bottomLeft[1] + 'px'
-        grid.appendChild(block)
+        allblocks_div.appendChild(block)
     }
 }
 addBlocks()
+
 //add user
 const user = document.createElement('div')
 user.classList.add('user')
 drawUser()
 grid.appendChild(user)
+
 //draw the user
 function drawUser() {
     user.style.left = currentPosition[0] + 'px'
     user.style.bottom = currentPosition[1] + 'px'
 }
+
 //draw the ball
 function drawBall() {
     ball.style.left = ballCurrentPosition[0] + 'px'
     ball.style.bottom = ballCurrentPosition[1] + 'px'
 }
+
 //move user
 function moveUser(event) {
     switch (event.key) {
@@ -84,19 +101,69 @@ function moveUser(event) {
             break;
     }
 }
+
 document.addEventListener('keydown', moveUser)
+
 //add ball
 const ball = document.createElement('ball')
 ball.classList.add('ball')
 drawBall()
 grid.appendChild(ball)
+
 function moveBall() {
     ballCurrentPosition[0] += xDirection
     ballCurrentPosition[1] += yDirection
     drawBall()
     checkCollisions()
 }
-timerId = setInterval(moveBall, 30)
+// timerId = setInterval(moveBall, 20)
+
+function frameAnimation() {
+
+    requestAnimationFrame(frameAnimation)
+
+    if (game_over == false) {
+        if (isPaused == false) {
+            moveBall()
+        }
+    }
+
+    if (game_over == true) {
+        document.querySelector('#pause').disabled = true
+    }
+}
+
+let animationId = requestAnimationFrame(frameAnimation)
+
+document.querySelector('#pause').onclick = function () {
+    if (isPaused == false) {
+        pause.innerHTML = "continue"
+    } else {
+        pause.innerHTML = "pause"
+    }
+    isPaused = !isPaused // isPaused = true
+}
+
+document.querySelector('#reset').onclick = function () {
+    let allblocks = Array.from(allblocks_div.querySelectorAll('div'))
+    if (game_reset == true) {
+        xDirection
+        yDirection
+        for (let i = 0; i < allblocks.length; i++) {
+            allblocks[i].classList.add('block')
+        }
+        // console.log(allblocks_div.querySelectorAll('div'))
+        console.log(Array.from(allblocks_div.querySelectorAll('div')))
+
+        // allBlocks[i].classList.remove('block')
+        // addBlocks[i].classList.add('grid')
+        console.log(allblocks_div)
+        console.log(blocks)
+        drawUser()
+
+    }
+}
+
 //check for collisios
 function checkCollisions() {
     //check for block collisions
@@ -110,33 +177,53 @@ function checkCollisions() {
             blocks.splice(i, 1)
             changeDirection()
             score++
-            scoreDisplay.innerHTML = score
+            scoreDisplay.innerHTML = 'Score: ' + score
+
             //check for win
             if (blocks.length === 0) {
                 scoreDisplay.innerHTML = 'u win'
-                clearInterval(timerId)
+                clearAnimationFrame(animationId)
                 document.removeEventListener('keydown', moveUser)
+                return
             }
         }
     }
+
     //check for wall collisions
     if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[1] >= (boardHeight - ballDiameter) || ballCurrentPosition[0] <= 0) {
         changeDirection()
     }
-    //chreck for user collisions
+
+    //check for user collisions
     if (
         (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) && (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight)
     ) {
         changeDirection()
     }
 
+
     //check for game over
     if (ballCurrentPosition[1] <= 0) {
         clearInterval(timerId)
-        scoreDisplay.innerHTML = 'u lose'
-        document.removeEventListener('keydown', moveUser)
+        lives--
+        xDirection = -2
+        yDirection = 2
+        cancelAnimationFrame(animationId)
+        if (lives >= 0) {
+            live.innerHTML = 'Lives: ' + lives
+            // animationId = requestAnimationFrame(checkCollisions);
+        }
+        if (lives == 0) {
+            game_over = true
+            scoreDisplay.innerHTML = "Game over"
+            document.removeEventListener('keydown', moveUser)
+            return
+        }
     }
+
 }
+
+
 //change direction
 function changeDirection() {
     if (xDirection === 2 && yDirection === 2) {
@@ -155,4 +242,5 @@ function changeDirection() {
         xDirection = 2
         return
     }
+
 }
